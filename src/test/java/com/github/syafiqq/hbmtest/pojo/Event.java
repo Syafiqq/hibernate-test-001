ad52f8f563e756e15645b52943d1ac05d31b57c6
Syafiq <syafiq.rezpector@gmail.com>
2019-02-08 06:34:41 +0700

    Add event class

---
+++ src/test/java/com/github/syafiqq/hbmtest/pojo/Event.java
@@ -0,0 +1,24 @@
+package com.github.syafiqq.hbmtest.pojo;
+
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/*
+ * This <test-hibernate> created by :
+ * Name         : syafiq
+ * Date / Time  : 08 February 2019, 6:30 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class Event
+{
+    @Getter @Setter private Long id;
+    @Getter @Setter private String title;
+    @Getter @Setter private Date date;
+}
