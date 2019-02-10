8fa54265bdfed1c8c5901d919a5989fc05028324
Syafiq <syafiq.rezpector@gmail.com>
2019-02-10 12:16:15 +0700

    Add attendee

---
+++ src/main/java/com/github/syafiqq/hbmtest/pojo/Attendee.java
@@ -0,0 +1,34 @@
+package com.github.syafiqq.hbmtest.pojo;
+
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/*
+ * This <test-hibernate> created by :
+ * Name         : syafiq
+ * Date / Time  : 10 February 2019, 12:12 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Attendee
+{
+    private Long id;
+    private String name;
+    private Date date;
+    private Long eventId;
+    private Event event;
+
+    @java.beans.ConstructorProperties({"name", "date", "event"})
+    public Attendee(String name, Date date, Event event)
+    {
+        this.name = name;
+        this.date = date;
+        this.eventId = event.getId();
+        this.event = event;
+    }
+}
