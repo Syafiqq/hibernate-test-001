39afe1e44d6588b20344cd64afe4ff90d840f9dd
Syafiq <syafiq.rezpector@gmail.com>
2019-02-10 11:41:26 +0700

    Add h2 basic test

---
+++ src/test/java/com/github/syafiqq/hbmtest/db/h2/BasicTest.java
@@ -0,0 +1,19 @@
+package com.github.syafiqq.hbmtest.db.h2;
+
+import com.github.syafiqq.hbmtest.db.common.CommonDbTest;
+
+/*
+ * This <test-hibernate> created by :
+ * Name         : syafiq
+ * Date / Time  : 09 February 2019, 8:00 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+public class BasicTest extends CommonDbTest
+{
+    @SuppressWarnings("MethodDoesntCallSuperMethod")
+    @Override protected void setUp() throws Exception
+    {
+        super.sessionFactory = super.setUp("/hbm/config/hibernate.h2.cfg.xml");
+    }
+}
