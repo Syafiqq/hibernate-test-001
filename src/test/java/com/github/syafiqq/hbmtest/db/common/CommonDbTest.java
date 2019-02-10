f96427f4629a7adcde195a4b77df871ae9f686c2
Syafiq <syafiq.rezpector@gmail.com>
2019-02-10 11:40:23 +0700

    Change to Abstract connection

--- src/test/java/com/github/syafiqq/hbmtest/db/common/CommonDbTest.java
+++ src/test/java/com/github/syafiqq/hbmtest/db/common/CommonDbTest.java
@@ -1,13 +1,12 @@
 package com.github.syafiqq.hbmtest.db.common;
 
+import com.github.syafiqq.hbmtest.db.AbstractConnection;
 import com.github.syafiqq.hbmtest.pojo.Event;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import junit.framework.TestCase;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.junit.Assert;
 
 /*
@@ -17,25 +16,14 @@ import org.junit.Assert;
  * Email        : syafiq.rezpector@gmail.com
  * Github       : syafiqq
  */
-public class CommonDbTest extends TestCase
+public class CommonDbTest extends TestCase implements AbstractConnection
 {
     protected SessionFactory sessionFactory;
 
     @Override
     protected void setUp() throws Exception
     {
-        final var registry = new StandardServiceRegistryBuilder()
-                .configure("/hbm/config/hibernate.h2.cfg.xml") // configures settings from hibernate.h2.cfg.xml
-                .build();
-        try
-        {
-            sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
-        }
-        catch(Exception e)
-        {
-            System.err.println(e.getMessage());
-            StandardServiceRegistryBuilder.destroy(registry);
-        }
+        sessionFactory = AbstractConnection.super.setUp("/hbm/config/hibernate.h2.cfg.xml");
     }
 
     @Override
