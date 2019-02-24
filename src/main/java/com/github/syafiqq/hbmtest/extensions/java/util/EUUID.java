a155b0e49a9fc1002cceb9c04d1f5b7a993d5474
Syafiq <syafiq.rezpector@gmail.com>
2019-02-24 12:38:46 +0700

    Add UUID Extension

---
+++ src/main/java/com/github/syafiqq/hbmtest/extensions/java/util/EUUID.java
@@ -0,0 +1,13 @@
+package com.github.syafiqq.hbmtest.extensions.java.util;
+
+/*
+ * This <test-hibernate> created by :
+ * Name         : syafiq
+ * Date / Time  : 24 February 2019, 12:37 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+public class EUUID
+{
+
+}
