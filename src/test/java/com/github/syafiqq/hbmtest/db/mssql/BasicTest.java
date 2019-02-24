5e75ea8792669734cb4c7a9bc8dd877960d117c6
Syafiq <syafiq.rezpector@gmail.com>
2019-02-24 11:56:52 +0700

    Add hibernate mssql basic test

---
+++ src/test/java/com/github/syafiqq/hbmtest/db/mssql/BasicTest.java
@@ -0,0 +1,17 @@
+package com.github.syafiqq.hbmtest.db.mssql;
+
+/*
+ * This <test-hibernate> created by :
+ * Name         : syafiq
+ * Date / Time  : 09 February 2019, 8:00 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+public class BasicTest extends com.github.syafiqq.hbmtest.db.common.BasicTest
+{
+    @SuppressWarnings("MethodDoesntCallSuperMethod")
+    @Override protected void setUp() throws Exception
+    {
+        super.sessionFactory = super.setUp("/hbm/config/hibernate.mssql.cfg.xml");
+    }
+}
