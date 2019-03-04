f615fcd28ce824df4bf739022eef80aacbca7298
Syafiq <syafiq.rezpector@gmail.com>
2019-03-05 03:26:04 +0700

    Add Address Pojo

---
+++ src/main/java/com/github/syafiqq/hbmtest/pojo/Address.java
@@ -0,0 +1,17 @@
+package com.github.syafiqq.hbmtest.pojo;
+
+import java.util.UUID;
+
+/*
+ * This <test-hibernate> created by :
+ * Name         : syafiq
+ * Date / Time  : 05 March 2019, 3:24 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+public class Address
+{
+    private UUID id;
+    private String City;
+    private String State;
+}
