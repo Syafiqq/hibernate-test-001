20e7212e4d33fe5755a9ec508d8496086afcac5f
Syafiq <syafiq.rezpector@gmail.com>
2019-03-05 03:26:11 +0700

    Add User Pojo

---
+++ src/main/java/com/github/syafiqq/hbmtest/pojo/User.java
@@ -0,0 +1,18 @@
+package com.github.syafiqq.hbmtest.pojo;
+
+import java.util.UUID;
+
+/*
+ * This <test-hibernate> created by :
+ * Name         : syafiq
+ * Date / Time  : 05 March 2019, 3:23 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+public class User
+{
+    private UUID id;
+    private String name;
+    private Integer age;
+    private Address address;
+}
