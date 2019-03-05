342805265158d76bfa477134660399c0c88308eb
Syafiq <syafiq.rezpector@gmail.com>
2019-03-06 06:33:28 +0700

    Add DirtyDataInspector

---
+++ src/test/java/com/github/syafiqq/hbmtest/util/DirtyDataInspector.java
@@ -0,0 +1,36 @@
+package com.github.syafiqq.hbmtest.util;
+
+import com.github.syafiqq.hbmtest.pojo.Address;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import org.hibernate.EmptyInterceptor;
+import org.hibernate.type.Type;
+
+/*
+ * This <test-hibernate> created by :
+ * Name         : syafiq
+ * Date / Time  : 06 March 2019, 6:15 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+public class DirtyDataInspector extends EmptyInterceptor
+{
+    private static final ArrayList<Address> dirtyEntities = new ArrayList<>();
+
+    public static List<Address> getDirtyEntities()
+    {
+        return dirtyEntities;
+    }
+
+    public static void clearDirtyEntitites()
+    {
+        dirtyEntities.clear();
+    }
+
+    @Override public boolean onFlushDirty(Object entity, Serializable id, Object[] currentState, Object[] previousState, String[] propertyNames, Type[] types)
+    {
+        dirtyEntities.add((Address) entity);
+        return true;
+    }
+}
