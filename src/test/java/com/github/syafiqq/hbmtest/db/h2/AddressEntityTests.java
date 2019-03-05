0e470cd04cf251bdda29e7d34568f9bb6424883a
Syafiq <syafiq.rezpector@gmail.com>
2019-03-06 06:08:01 +0700

    Add Address Entity Test

---
+++ src/test/java/com/github/syafiqq/hbmtest/db/h2/AddressEntityTests.java
@@ -0,0 +1,24 @@
+package com.github.syafiqq.hbmtest.db.h2;
+
+import com.github.syafiqq.hbmtest.db.AbstractConnection;
+import junit.framework.TestCase;
+import org.hibernate.SessionFactory;
+
+/*
+ * This <test-hibernate> created by :
+ * Name         : syafiq
+ * Date / Time  : 06 March 2019, 6:06 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+public class AddressEntityTests extends TestCase implements AbstractConnection
+{
+    @SuppressWarnings("WeakerAccess")
+    protected SessionFactory sessionFactory;
+
+    @Override
+    protected void setUp() throws Exception
+    {
+        sessionFactory = AbstractConnection.super.setUp("/hbm/config/hibernate.h2.cfg.xml");
+    }
+}
