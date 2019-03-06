af0cb360fc004e40f3f47d3479845ce02abcc618
Syafiq <syafiq.rezpector@gmail.com>
2019-03-06 23:16:05 +0700

    Add one on one foreign test

--- src/test/java/com/github/syafiqq/hbmtest/db/h2/AddressEntityTests.java
+++ src/test/java/com/github/syafiqq/hbmtest/db/h2/AddressEntityTests.java
@@ -1,8 +1,12 @@
 package com.github.syafiqq.hbmtest.db.h2;
 
 import com.github.syafiqq.hbmtest.db.AbstractConnection;
+import com.github.syafiqq.hbmtest.db.common.Storage;
+import com.github.syafiqq.hbmtest.util.HibernateLifecycleUtil;
 import junit.framework.TestCase;
+import static org.hamcrest.CoreMatchers.is;
 import org.hibernate.SessionFactory;
+import org.junit.Assert;
 
 /*
  * This <test-hibernate> created by :
@@ -28,6 +32,24 @@ public class AddressEntityTests extends TestCase implements AbstractConnection
         if(sessionFactory != null)
         {
             sessionFactory.close();
+        }
+    }
+
+    public void testGetManagedEntities()
+    {
+        try(var session = sessionFactory.openSession())
+        {
+            var user = Storage.users.get(1L);
+            var address = Storage.addresses.get(1L);
+            user.setAddress(address);
+            address.setUser(user);
+            address.setId(user.getId());
+            Assert.assertThat(0, is(HibernateLifecycleUtil.getManagedEntities(session).size()));
+            session.beginTransaction();
+            session.save(user);
+            session.getTransaction()
+                   .commit();
+            Assert.assertThat(2, is(HibernateLifecycleUtil.getManagedEntities(session).size()));
         }
     }
 }
