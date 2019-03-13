64f5cac0f764ffea388a727230280d69135a460c
Syafiq <syafiq.rezpector@gmail.com>
2019-03-14 06:50:19 +0700

    Add address 2 to user

--- src/main/java/com/github/syafiqq/hbmtest/pojo/User.java
+++ src/main/java/com/github/syafiqq/hbmtest/pojo/User.java
@@ -23,16 +23,30 @@ public class User
     private String name;
     private Integer age;
     private transient Address1 address1;
+    private transient Address2 address2;
 
     @java.beans.ConstructorProperties({"name", "age"})
     public User(String name, Integer age)
     {
-        this(UUIDs.timeBased(), name, age, null);
+        this(UUIDs.timeBased(), name, age, null, null);
     }
 
     @java.beans.ConstructorProperties({"name", "age", "address1"})
     public User(String name, Integer age, Address1 address1)
     {
-        this(UUIDs.timeBased(), name, age, address1);
+        this(UUIDs.timeBased(), name, age, address1, null);
     }
+
+    @java.beans.ConstructorProperties({"name", "age", "address2"})
+    public User(String name, Integer age, Address2 address2)
+    {
+        this(UUIDs.timeBased(), name, age, null, address2);
+    }
+
+    @java.beans.ConstructorProperties({"name", "age", "address1", "address2"})
+    public User(String name, Integer age, Address1 address1, Address2 address2)
+    {
+        this(UUIDs.timeBased(), name, age, address1, address2);
+    }
+
 }
